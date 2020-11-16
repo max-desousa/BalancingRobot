077f7621df6644e1ea70e901f5d5dd59e3e71f99
Max De Sousa <max.de.sousa@outlook.com>
2020-11-15 22:18:26 -0500

    Here is the most basic code that should facilitate self balance
    Here the 2 pwm's have been used to get the bi-directional ESC to drive
    the motors. The Kp, Ki, and Kd are all for testing.
    
    It was discovered in the testing from here though that the motors I had
    selected for this method (brushless outrunners) cannot supply enough
    torque. Upon applying the full weight of the device, the motors
    practically stall.
    
    I may be able to set up my PSU to allow for more amperage. I put a 5 amp
    cap on the 5V supply and it didn't work. The ESC supports up to 20A, but
    I don't want to use that much power, and don't think the torqe can be
    overcome.

--- main/main.ino
+++ main/main.ino
@@ -15,6 +15,7 @@ Servo LeftMotor;
 Servo RightMotor;
 MPU9250 myIMU(MPU9250_ADDRESS, ITCPort, I2CClock);
 
+
 int LeftMotorPin = 7;
 int RightMotorPin = 8;
 
@@ -30,6 +31,17 @@ bool ReverseRightMotorDirection = false;
 int StoppedSpeed = 91;
 int MaxForwardSpeed = 106;
 int MaxBackwardSpeed = 76;
+
+double CurrentAngle, AbsoluteValueOfCurrentAngle, NegativeMotorOffset, PositiveMotorOffset;
+double TargetAngle = 0;
+double Kp = 2;
+double Ki = 0.05;
+double Kd = 0.01;
+
+int FinalESCServoSpeed = 0;
+
+PID negativeAnglePID(&CurrentAngle, &NegativeMotorOffset, &TargetAngle, Kp, Ki, Kd, DIRECT);
+PID positiveAnglePID(&CurrentAngle, &PositiveMotorOffset, &TargetAngle, Kp, Ki, Kd, REVERSE);
 
 
 //====================================
@@ -59,37 +71,15 @@ void setup()
   myIMU.getAres();
   myIMU.getGres();
   myIMU.getMres();
-}
 
-void initializeMotorsToStoppedPosition()
-{
-  LeftMotor.attach(LeftMotorPin);
-  RightMotor.attach(RightMotorPin);
+  positiveAnglePID.SetOutputLimits(0,15);
+  negativeAnglePID.SetOutputLimits(0,15);
   
-  bool leftMotorInitialized = false;
-  bool rightMotorInitialized = false;
-
-  while (!leftMotorInitialized || !rightMotorInitialized)
-  {
-    if (LeftMotor.attached() && LeftMotor.read() == StoppedSpeed)
-    {
-      leftMotorInitialized = true;
-    }
-    else
-    {
-      LeftMotor.write(StoppedSpeed);
-    }
-    if (RightMotor.attached() && RightMotor.read() == StoppedSpeed)
-    {
-      rightMotorInitialized = true;
-    }
-    else
-    {
-      RightMotor.write(StoppedSpeed);
-    }
-    
-  }
+  positiveAnglePID.SetMode(AUTOMATIC);
+  negativeAnglePID.SetMode(AUTOMATIC);
+  
 }
+
 
 
 //====================================
@@ -103,10 +93,21 @@ void initializeMotorsToStoppedPosition()
 void loop()
 {
   updateAngleData();
+  CurrentAngle = myIMU.pitch;
 
+  negativeAnglePID.Compute();
+  positiveAnglePID.Compute();
   
-  Serial.print("Pitch: ");
+  FinalESCServoSpeed = StoppedSpeed + PositiveMotorOffset - NegativeMotorOffset;
+
+  Serial.print("My motor speed needs to be: ");
+  Serial.println(FinalESCServoSpeed);
+  
+  Serial.print("\tPitch: ");
   Serial.println(myIMU.pitch);
+
+  LeftMotor.write(FinalESCServoSpeed);
+  RightMotor.write(FinalESCServoSpeed);
 }
 
 
@@ -120,7 +121,44 @@ void loop()
 
 
 
+void initializeMotorsToStoppedPosition()
+{
+  LeftMotor.attach(LeftMotorPin);
+  RightMotor.attach(RightMotorPin);
+  
+  bool leftMotorInitialized = false;
+  bool rightMotorInitialized = false;
 
+  while(leftMotorInitialized != true || rightMotorInitialized != true)
+  {
+    if(LeftMotor.attached() == false)
+    {
+      LeftMotor.attach(LeftMotorPin);
+    }
+    if(RightMotor.attached() == false)
+    {
+      RightMotor.attach(RightMotorPin);
+    }
+    
+    if(LeftMotor.attached() == true && LeftMotor.read() != StoppedSpeed)
+    {
+      LeftMotor.write(StoppedSpeed);
+    }
+    if(RightMotor.attached() == true && RightMotor.read() != StoppedSpeed)
+    {
+      RightMotor.write(StoppedSpeed);
+    }
+
+    if(LeftMotor.attached() == true && LeftMotor.read() == StoppedSpeed)
+    {
+      leftMotorInitialized = true;
+    }
+    if(RightMotor.attached() == true && RightMotor.read() == StoppedSpeed)
+    {
+      rightMotorInitialized = true;
+    }
+  }
+}
 
 void updateAngleData()
 {
